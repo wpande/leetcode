9893833ae38b9d08ce6de4f7e5647df8ef4e3e09
wpande <wp11111111@gmail.com>
2015-06-21 21:39:41 +0800

    delete hashmap

--- scramblestring.java
+++ scramblestring.java
@@ -1,32 +1,10 @@
 public class Solution {
-    boolean setequal(String s1, String s2){
-        int len1=s1.length();
-        int len2=s2.length();
-        if(len1!=len2)
-            return false;
-        HashMap<Character, Integer> hm1=new HashMap<Character, Integer>();
-        HashMap<Character, Integer> hm2=new HashMap<Character, Integer>();
-        for(int i=0;i<len1;i++){
-            char ch=s1.charAt(i);
-            if(hm1.containsKey(ch))
-                hm1.put(ch,hm1.get(ch)+1);
-            else
-                hm1.put(ch,1);
-            ch=s2.charAt(i);
-            if(hm2.containsKey(ch))
-                hm2.put(ch,hm2.get(ch)+1);
-            else
-                hm2.put(ch,1);
-        }
-        return hm1.equals(hm2);
-    }
-    
     public boolean isScramble(String s1, String s2) {
         int len1=s1.length();
         int len2=s2.length();
         if(len1==0&&len2==0)//null string
             return true;
-        if(len1!=len2||(len1==1&&s1.charAt(0)!=s2.charAt(0))||(!setequal(s1,s2)))
+        if(len1!=len2||(len1==1&&s1.charAt(0)!=s2.charAt(0)))
             return false;
         boolean [][][]res=new boolean[len1][len1][len1];
         int l=1;
